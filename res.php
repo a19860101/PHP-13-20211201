e465e2663c5e276f894ecec58d0e90ecc123e4b4
Zac <a19860101@gmail.com>
2021-12-08 20:20:40 +0800

    20211208

--- res.php
+++ res.php
@@ -17,7 +17,7 @@
     //     echo $_POST['mail'];
     // }
     // extract($_POST);
-    extract($_REQUEST);
+    // extract($_REQUEST);
     // echo $_REQUEST['name'];
 
     // echo trim($name);
@@ -26,15 +26,8 @@
     // var_dump(htmlspecialchars($name));//< > " ' &
     // var_dump(ctype_space($name));
   
-    function check($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-    $name = check($name);
-    var_dump($name);
+    // $name = check($name);
+    // var_dump($name);
 
     // var_dump($name);
     // echo '<br>';
@@ -52,15 +45,28 @@
        
     // var_dump($_POST);
     // var_dump($_REQUEST);
-
-    
-    
-    
     
     // header('refresh:5;url=form.php');
 
     // header('location:form.php');
-        
+    
+    function check($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
        
-    
-    
+    extract($_REQUEST);
+    if(empty($name) || ctype_space($name)){
+        echo 'name欄位必填';    
+    }else{
+        $name = check($name);
+        echo $name;
+    }
+    if(empty($mail) || ctype_space($mail)){
+        echo 'mail欄位必填';    
+    }else{
+        $mail = check($mail);
+        echo $mail;
+    }
