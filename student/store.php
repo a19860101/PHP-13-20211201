aea368bd63b365503ebe03464bbeb7d1f67d8f9e
Zac <a19860101@gmail.com>
2021-12-13 20:28:09 +0800

    20211213

--- student/store.php
+++ student/store.php
@@ -1,2 +1,9 @@
 <?php
     include('db.php');
+    extract($_REQUEST);
+    $skill = implode(',',$skill);
+    $sql = "INSERT INTO students(name,mail,phone,gender,edu,skill,content)
+            VALUES('$name','$mail','$phone','$gender','$edu','$skill','$content')";
+    mysqli_query($db,$sql);
+
+    header('location:index.php');
