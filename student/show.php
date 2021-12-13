b0617541adc3d0d4d8c00919c2d138af8203ee15
Zac <a19860101@gmail.com>
2021-12-13 20:38:51 +0800

    20211213

--- student/show.php
+++ student/show.php
@@ -1,6 +1,7 @@
 <?php
     include('db.php');
-    $id = $_GET['id'];
+    // $id = $_GET['id'];
+    extract($_REQUEST);
     $sql = "SELECT * FROM students WHERE id = {$id}";
 
     $result = mysqli_query($db,$sql);
