32ffd764751b1248770885adbfe1a3d7bd15bf22
Zac <a19860101@gmail.com>
2021-12-13 21:10:15 +0800

    20211213

--- student/show.php
+++ student/show.php
@@ -5,5 +5,26 @@
     $sql = "SELECT * FROM students WHERE id = {$id}";
 
     $result = mysqli_query($db,$sql);
-
-    print_r($result);
+    $data = mysqli_fetch_assoc($result);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>姓名:<?php echo $data['name'];?></li>
+        <li>電話:<?php echo $data['phone'];?></li>
+        <li>Mail:<?php echo $data['mail'];?></li>
+        <li>學歷:<?php echo $data['edu'];?></li>
+        <li>性別:<?php echo $data['gender'];?></li>
+        <li>專長:<?php echo $data['skill'];?></li>
+        <li>備註:<?php echo $data['content'];?></li>
+    </ul>
+    <a href="index.php">學員列表</a>
+</body>
+</html>
