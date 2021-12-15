b5d9640fe57ac4d655f22eba430aa5f2283d8202
Zac <a19860101@gmail.com>
2021-12-15 21:28:16 +0800

    20211215

--- student_pdo/update.php
+++ student_pdo/update.php
@@ -1,26 +1,16 @@
 <?php
-    include('db.php');
+    include('pdo.php');
     extract($_REQUEST);
 
     $skill = implode(',',$skill);
 
-    // 方法一
-    // $sql = "UPDATE students SET 
-    //         name='$name',
-    //         mail='$mail',
-    //         phone='$phone',
-    //         gender='$gender',
-    //         edu='$edu',
-    //         skill='$skill',
-    //         content='$content'
-    //         WHERE id = {$id}
-    //         ";
-    // mysqli_query($db,$sql);
-
-    //方法二
     $sql = "UPDATE students SET name=?,mail=?,phone=?,gender=?,edu=?,skill=?,content=? WHERE id =?";
-    $stmt = $db->prepare($sql);
-    $stmt->bind_param('sssssssi',$name,$mail,$phone,$gender,$edu,$skill,$content,$id);
-    $stmt->execute();
+    try{
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$name,$mail,$phone,$gender,$edu,$skill,$content,$id]);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
     
     header('location:show.php?id='.$id);
