777e89e6d3976e731190eaffa628c25bf53f822f
Zac <a19860101@gmail.com>
2021-12-15 19:57:01 +0800

    20211215

--- student/delete.php
+++ student/delete.php
@@ -1,8 +1,14 @@
 <?php
     include('db.php');
     extract($_REQUEST);
+    //方法一
+    // $sql = "DELETE FROM students WHERE id = {$id}";
+    // mysqli_query($db,$sql);
 
-    $sql = "DELETE FROM students WHERE id = {$id}";
-    mysqli_query($db,$sql);
+    //方法二
+    $sql = "DELETE FROM students WHERE id = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('i',$id);
+    $stmt->execute();
 
     header('location:index.php');
