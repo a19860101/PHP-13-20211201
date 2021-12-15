1b3d03c47e424e19f81118c9b65eb1ad07b7ebac
Zac <a19860101@gmail.com>
2021-12-15 19:31:01 +0800

    20211215

--- student/store.php
+++ student/store.php
@@ -2,8 +2,20 @@
     include('db.php');
     extract($_REQUEST);
     $skill = implode(',',$skill);
-    $sql = "INSERT INTO students(name,mail,phone,gender,edu,skill,content)
-            VALUES('$name','$mail','$phone','$gender','$edu','$skill','$content')";
-    mysqli_query($db,$sql);
+    //方法一
+//     $sql = "INSERT INTO students(name,mail,phone,gender,edu,skill,content)
+//             VALUES('$name','$mail','$phone','$gender','$edu','$skill','$content')";
+//     mysqli_query($db,$sql);
 
-    header('location:index.php');
+
+	//方法二  
+	$sql = "INSERT INTO students(name,mail,phone,gender,edu,skill,content)VALUES(?,?,?,?,?,?,?)";
+	
+	//預備陳述式
+	$stmt = $db->prepare($sql);
+	$stmt->bind_param('sssssss',$name,$mail,$phone,$gender,$edu,$skill,$content);
+	$stmt->execute();
+
+
+
+	header('location:index.php');
