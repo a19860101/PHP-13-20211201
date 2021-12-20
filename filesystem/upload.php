b41c9789f279788a8d68b54f03ffc97a8600dea0
Zac <a19860101@gmail.com>
2021-12-20 19:27:31 +0800

    20211220

--- filesystem/upload.php
+++ filesystem/upload.php
@@ -7,13 +7,22 @@
     if(!is_dir('images')){
         mkdir('images');
     }
+
+    //副檔名
     $ext = pathinfo($name,PATHINFO_EXTENSION);
     // echo $ext;
+
+    //檔名
     $imgName =md5(time());
     // $imgName = md5(uniqid());
-
+    //完整檔名
     $fullname = $imgName.'.'.$ext;
-    echo $fullname;
-    // if($error == 0){
-        // move_uploaded_file($tmp_name,'images/'.$name);
-    // }
+
+    if($ext != 'jpg' && $ext!= 'gif' && $ext !='png' && $ext!='jpeg'){
+        echo '<script>alert("請上傳正確的圖檔格式!")</script>';
+        header('refresh:0;url=form.php');
+        return;
+    }
+    if($error == 0){
+        move_uploaded_file($tmp_name,'images/'.$name);
+    }
