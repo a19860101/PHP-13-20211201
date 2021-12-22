b27f32e43645fb4c6f89e7f068f09117d722c80f
Zac <a19860101@gmail.com>
2021-12-22 21:02:08 +0800

    20211222

--- member/store.php
+++ member/store.php
@@ -3,6 +3,17 @@
     include('pdo.php');
 
     extract($_REQUEST);
+
+    //檢查有沒有重複的會員名稱
+    $sql_check = 'SELECT * FROM users WHERE user = ?';
+    $stmt_check = $pdo->prepare($sql_check);
+    $stmt_check->execute([$user]);
+    if($stmt_check->rowCount() > 0){
+        echo '<script>alert("此帳號名稱已被使用，請重新設定!")</script>';
+        header('refresh:0;url=create.php');
+        return; 
+    }
+
     $pw = md5(sha1($pw));
     $sql = 'INSERT INTO users(user,pw,created_at)VALUES(?,?,?)';
     $now = date('Y-m-d H:i:s');
