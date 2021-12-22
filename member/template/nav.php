b97608d6ff5c7c50f006bf6c9cd19f39893e5a74
Zac <a19860101@gmail.com>
2021-12-22 20:32:02 +0800

    20211222

--- member/template/nav.php
+++ member/template/nav.php
@@ -1,3 +1,8 @@
+<?php
+    if(!session_id()){
+        session_start();
+    }
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Navbar</a>
@@ -6,15 +11,22 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
+                <?php if(!isset($_SESSION['AUTH'])){ ?>
                 <li class="nav-item">
                     <a class="nav-link" href="create.php">申請會員</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="login.php">登入</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">登出</a>
-                </li>
+                <?php } ?>
+                <?php if(isset($_SESSION['AUTH'])){ ?>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"><?php echo $_SESSION['AUTH']['user']?>你好</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">登出</a>
+                    </li>
+                <?php } ?>
             </ul>
         </div>
     </div>
