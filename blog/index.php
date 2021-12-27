960e0ef9ccef04c01f1b2e23b63b753367a830e2
Zac <a19860101@gmail.com>
2021-12-27 21:40:06 +0800

    20211227

--- blog/index.php
+++ blog/index.php
@@ -5,10 +5,17 @@
 <?php include('template/header.php'); ?>
 <?php include('template/nav.php'); ?>
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
         <?php foreach($posts as $post){ ?>
         <div class="col-lg-8">
-            <h2><?php echo $post['title'];?></h2>
+            <div class="border rounded-3 p-3 mb-3">
+                <h2><?php echo $post['title'];?></h2>
+                <div class="mb-3">
+                    <?php echo $post['content']; ?>
+                </div>
+                <a href="#" class="btn btn-primary btn-sm">繼續閱讀</a>
+                <span class="float-end mt-3">最後更新時間：<?php echo $post['updated_at'];?></span>
+            </div>
         </div>
         <?php } ?>
     </div>
