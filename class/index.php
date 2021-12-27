47032394b7c3c3fa857eb61ff14e3a77a23e9125
Zac <a19860101@gmail.com>
2021-12-27 19:46:11 +0800

    20211227

--- class/index.php
+++ class/index.php
@@ -3,6 +3,8 @@
 include('hello/User.php');
 include('hello/Role.php');
 
+use hello\Role\Show;
+
 $a = new hello\User\Show;
 echo $a->walk();
 
@@ -10,3 +12,8 @@ echo '<br>';
 
 $b = new hello\Role\Show;
 echo $b->walk();
+
+echo '<br>';
+
+$c = new Show;
+echo $c->walk();
