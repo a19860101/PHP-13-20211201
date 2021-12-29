9096fe0af17641dbdbe5a99ceaf1d17ef367609f
Zac <a19860101@gmail.com>
2021-12-29 21:43:20 +0800

    20211229

--- blog/Post/create.php
+++ blog/Post/create.php
@@ -1,3 +1,7 @@
+<?php
+    include('../Controller/Category.php');
+    $datas = Controller\Category::all();
+?>
 <?php include('../template/header.php');?>
 <?php include('../template/nav.php');?>
 <div class="container">
@@ -9,11 +13,20 @@
         <div class="col-8">
             <form action="store.php" method="post">
                 <div class="mb-3">
-                    <label for="title">文章標題</label>
+                    <label for="title" class="form-label">文章標題</label>
                     <input type="text" id="title" name="title" class="form-control">
                 </div>
                 <div class="mb-3">
-                    <label for="">文章內文</label>
+                    <label for="" class="form-label">文章分類</label>
+                    <select name="category_id" id="" class="form-select">
+                        <option>--請選擇--</option>
+                        <?php foreach($datas as $data){ ?>
+                        <option value="<?php echo $data['id'];?>"><?php echo $data['title'];?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">文章內文</label>
                     <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
                 </div>
                 <input type="submit" class="btn btn-primary" value="新增文章">
